38405faa16944b17335053d4711002544984b2a1
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-09 15:03:25 +0530

    creating nat gate way

---
+++ local.tf
@@ -0,0 +1,4 @@
+locals {
+  tags = merge(var.tags, { tf-module-name = "alb"}, {env = var.env})
+  lb_name = var.internal ? "${var.env}-alb-internal" : "${var.env}-alb-public"
+}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "tags" {}
+variable "env" {}
+variable "internal" {}
+variable "lb_type" {}
+variable "sg_ingress_cidr" {}
+variable "sg_port" {}
+variable "subnets" {}
+variable "vpc_id" {}
